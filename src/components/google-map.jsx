aea6afbf0d36f48f5c2a80f9a28efc660c4875c1
larry-xue <azoux0311@gmail.com>
2024-08-24 22:44:30 +0800

    fix: adjust map size

--- src/components/google-map.jsx
+++ src/components/google-map.jsx
@@ -1,12 +1,14 @@
 export default function GoogleMap({ coordinates }) {
   return (
+  <div className="w-full h-full p-2">
     <iframe
-      width="400"
-      height="400"
-      className="random-region-map"
+      width="100%"
+      height="600"
       loading="lazy"
       allowFullScreen={true}
       referrerPolicy="no-referrer-when-downgrade"
       src={`https://www.google.com/maps/embed/v1/view?key=${import.meta.env.VITE_MAP_API_KEY}&center=${coordinates.latitude},${coordinates.longitude}&zoom=18&maptype=satellite`}>
-    </iframe>)
+    </iframe>
+  </div>  
+  )
 }
